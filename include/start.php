d55ca75003df46546e83a0748f4ec8cc3c98e65e
John King <jnkisd@rit.edu>
2017-10-02 00:35:03 -0400

    Session cookie defense demo implemented

--- include/start.php
+++ include/start.php
@@ -1,4 +1,16 @@
 <?php
+    // DEMO: Session cookie security (secure & httponly flags)
+    /*
+    $cookieParams = session_get_cookie_params();
+    session_set_cookie_params(
+        $cookieParams['lifetime'],
+        $cookieParams['path'],
+        $cookieParams['domain'],
+        true,
+        true
+    );
+    */
+
     session_start();
     $GLOBALS['db'] = new PDO('sqlite:data/db');
     $GLOBALS['db']->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
