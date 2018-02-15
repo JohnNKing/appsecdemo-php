2e8def1fe58c0c7680210c5b21f5574b5abb0c39
John King <jnkisd@rit.edu>
2018-02-15 13:04:11 -0500

    Changing initial user (Admin) and default comments

--- data/create.sql
+++ data/create.sql
@@ -3,8 +3,9 @@ DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS comments;
 CREATE TABLE users (name TEXT);
 CREATE TABLE comments (id INTEGER PRIMARY KEY, comment TEXT, username TEXT);
-INSERT INTO users VALUES ('John');
-INSERT INTO comments (comment, username) VALUES ('Hello!', '');
+INSERT INTO users VALUES ('Admin');
+INSERT INTO comments (comment, username) VALUES ('Welcome!', 'Admin');
+INSERT INTO comments (comment, username) VALUES ('Hello', '');
 
 -- Hit Tracker
 DROP TABLE IF EXISTS hits;
