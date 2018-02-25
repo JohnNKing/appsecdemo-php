4f32a541f6ec5fa390c2969f25593549f69c7e5b
John King <jnkisd@rit.edu>
2018-02-25 16:21:51 -0500

    Adding HTTP Header demo

--- include/start.php
+++ include/start.php
@@ -6,6 +6,13 @@
     // $csp .= " connect-src 'self' https://tracker";
     // header($csp);
 
+    // DEMO: Other HTTP Headers
+    // header("X-Frame-Options: SAMEORIGIN");
+    // header("X-XSS-Protection: 1");
+    // header("X-Content-Type-Options: nosniff");
+    // header("Strict-Transport-Security: max-age=2592000");
+    // header("Referrer-Policy: strict-origin-when-cross-origin");
+
     // DEMO: Session cookie security (secure & httponly flags)
     // $cookieParams = session_get_cookie_params();
     // session_set_cookie_params(
